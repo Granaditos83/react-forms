dd361d5c1d96d1dfff5ea967b189f09061c48d67
Granaditos83 <cdgranados67360@umanizales.edu.co>
2021-04-28 22:06:29 -0500

    commit 28 Abril

--- src/pages/articulos/componentes/sb-dos/sb-dos.copm.jsx
+++ src/pages/articulos/componentes/sb-dos/sb-dos.copm.jsx
@@ -6,7 +6,29 @@ const SbDos = () => {
     return (
         <div className="sb-dos-container">
 
-<form>
+            <form>
+                <table>
+                    <tr>
+                    
+                    <td>Marcos bicicletas</td>
+                        <td><input type="text"/></td>
+                    </tr>
+                    <tr>
+                    
+                    <td>Accesorios para tu bicicleta</td>
+                        <td><input type="text"/></td>
+                    </tr>
+                   
+                    <tr>
+                        <td>Vestimenta deportiva</td>
+                        <td><input type="text"/></td>
+                    </tr>
+                    <button>Adicionar  producto</button>
+
+                        
+                        
+                    
+                </table>
                 <select>
                     <option value="Marcos bicicletas">Marcos bicicletas</option>
                     <option value="Accesorios para tu bicicleta">Accesorios para tu bicicleta</option>
